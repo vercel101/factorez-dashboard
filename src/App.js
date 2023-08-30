b726f13e52470eabd559d044782720f9e4bc063b
vercel101 <140298776+vercel101@users.noreply.github.com>
2023-08-30 11:43:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { useSelector } from "react-redux";
 import Admin from "./component/Admin";
-import { ToastContainer } from "react-toastify";
 function App() {
     const { darkModeReducer } = useSelector((state) => state);
     return (
         <div className={`${darkModeReducer && "dark"}`}>
-            <ToastContainer position={"top-center"} />
             <Admin />
             hello boss
         </div>
